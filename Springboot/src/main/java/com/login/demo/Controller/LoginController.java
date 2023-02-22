3663c9392215304ce307e2ff9b3bb0c0bc83e26f
MACintoshPH <argenteedmarkr@gmail.com>
2023-02-22 13:55:42 +0800

    Update LoginController.java

--- Springboot/src/main/java/com/login/demo/Controller/LoginController.java
+++ Springboot/src/main/java/com/login/demo/Controller/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 public class LoginController {
 
     @Autowired
